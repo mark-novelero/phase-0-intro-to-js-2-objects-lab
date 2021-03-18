1269ffeebdcf6af8f9dd12aae097b695a6a8ff5f
mark novelero <mark.novelero@gmail.com>
2021-03-18 18:39:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var employee = {
+    name: 'Mark', 
+    streetAddress: 'East 83rd'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    var newObj = {...employee}; 
+    newObj[key] = value; 
+    return newObj; 
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){   
+    employee[key] = value; 
+    return employee; 
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    var newObj = {...employee}; 
+    delete newObj[key]; 
+    return newObj; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]; 
+    return employee; 
+}
